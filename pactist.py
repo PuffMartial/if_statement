0da54d8917cc2a618fcbd77c41e6a3369e006a3a
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-09 10:35:42 +0200

    Add files via upload

---
+++ pactist.py
@@ -0,0 +1,15 @@
+is_authentication = False
+type_of_user = "random"
+
+if(type_of_user == "driver"):
+    print("hallo driver!")
+elif(type_of_user == "rider"):
+    print("hallo rider")
+elif(type_of_user == "employee"):
+    print("hallo employee")
+elif(type_of_user == "employer"):
+    print("hallo employer")
+elif(type_of_user == "root user"):
+    print("hallo root user")
+else:
+    print("please login")
